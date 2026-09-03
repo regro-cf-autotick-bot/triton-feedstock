76c0e66e7f1aa0fd32e4d30cc0065a5d6e1180d9
Michał Górny <mgorny@quansight.com>
2026-09-03 20:38:28 +0200

    Try using a staging output for LLVM
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,59 +31,6 @@ sed -i -e '/TRITON_BUILD_UT/s:\bON:OFF:' CMakeLists.txt
 # disk and link memory. LLVM is already built without -g (Release).
 sed -i -e '/FLAGS_TRITONRELBUILDWITHASSERTS/s: -g::' CMakeLists.txt
 
-CMAKE_HOST_ARGS=(
-    -DCMAKE_BUILD_TYPE=Release
-    -DLLVM_BUILD_UTILS=ON
-    -DLLVM_BUILD_TOOLS=OFF
-    -DLLD_BUILD_TOOLS=OFF
-    -DLLVM_BUILD_TELEMETRY=OFF
-    -DLLVM_ENABLE_PROJECTS="clang;mlir;lld"
-    -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU"
-    -DLLVM_ENABLE_TERMINFO=OFF
-    -DLLVM_INCLUDE_TESTS=OFF
-    -DMLIR_INCLUDE_TESTS=OFF
-    ${TRITON_BUILD_WITH_CCACHE:+-DLLVM_CCACHE_BUILD=ON}
-)
-
-# build LLVM first
-if [[ ${HOST} != ${BUILD} ]]; then
-    CMAKE_BUILD_ARGS=(
-        -DCMAKE_C_COMPILER="${CC_FOR_BUILD}"
-        -DCMAKE_CXX_COMPILER="${CXX_FOR_BUILD}"
-        -DCMAKE_BUILD_TYPE=Release
-        -DLLVM_ENABLE_ZSTD=OFF
-        -DLLVM_ENABLE_LIBXML2=OFF
-        -DLLVM_ENABLE_ZLIB=OFF
-        -DLLVM_ENABLE_PROJECTS="mlir"
-        ${TRITON_BUILD_WITH_CCACHE:+-DLLVM_CCACHE_BUILD=ON}
-    )
-    NATIVE_EXECUTABLES=(
-        llvm-tblgen
-        mlir-tblgen
-        mlir-linalg-ods-yaml-gen
-        mlir-src-sharder
-        mlir-pdll
-    )
-
-    cmake -G Ninja "${CMAKE_BUILD_ARGS[@]}" \
-        -Bllvm-project/build-native -Sllvm-project/llvm
-    cmake --build llvm-project/build-native -j "${MAX_JOBS}" \
-        -t "${NATIVE_EXECUTABLES[@]}"
-
-    NATIVE_BIN=$PWD/llvm-project/build-native/bin
-    CMAKE_HOST_ARGS+=(
-        -DCMAKE_CROSSCOMPILING=ON
-        -DLLVM_NATIVE_TOOL_DIR=$PWD/llvm-project/build-native/bin
-        #-DLLVM_TABLEGEN=$NATIVE_BIN/llvm-tblgen
-        #-DMLIR_TABLEGEN=$NATIVE_BIN/mlir-tblgen
-        #-DMLIR_LINALG_ODS_YAML_GEN
-    )
-fi
-
-cmake -G Ninja "${CMAKE_HOST_ARGS[@]}" \
-    -Bllvm-project/build -Sllvm-project/llvm
-cmake --build llvm-project/build -j "${MAX_JOBS}"
-
 export LLVM_SYSPATH=$PWD/llvm-project/build
 export LLVM_INCLUDE_DIRS=$LLVM_SYSPATH/include
 export LLVM_LIBRARY_DIR=$LLVM_SYSPATH/lib
